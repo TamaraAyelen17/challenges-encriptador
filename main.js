e0074b45d1da342962909f03bd90e2b714368143
TamiSorsaburu <tamysorsaburu@gmail.com>
2024-02-29 20:14:38 -0300

    Se agregan las funcion para encriptar y se agrega la funcion para el boton encriptar

---
+++ main.js
@@ -0,0 +1,24 @@
+const textArea = document.querySelector('.ingresar-texto');
+const mensaje = document.querySelector('.mensaje');
+const image = document.querySelector('.container-der');
+const parrafo = document.querySelector('.contenedor-parrafo');
+
+function encriptarLetras(texto){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    texto = texto.toLowerCase();
+
+    for(let i = 0 ; i < matrizCodigo.length; i++) {
+        if(texto.includes(matrizCodigo[i][0])){
+            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return texto;
+}
+
+function btnEncriptar() {
+    const textoEncriptado = encriptarLetras(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value ="";
+    image.style.backgroundImage = "none";
+    parrafo.style.display = "none";
+}
